d056e9c9c7974cadefc96afea6f533bd5f19f976
basokant <basokanthan@gmail.com>
2025-11-07 18:35:07 -0500

    add schema to db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,11 @@
 import { config } from "dotenv";
+import * as schema from "./schema.ts";
 import { drizzle } from "drizzle-orm/libsql";
 
 config({ path: ".env" });
 
 export const db = drizzle({
+  schema: schema,
   connection: {
     url: process.env.TURSO_CONNECTION_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
